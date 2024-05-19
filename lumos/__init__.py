97b5b35fcd44f084aeb75ecce4b714b7f0677a36
Adrian Rothenbuhler <adrian@redhill-embedded.com>
2024-05-18 19:49:46 -0600

    Bump version to 0.0.3

--- lumos/__init__.py
+++ lumos/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.2"   # This will be replaced by setuptools_scm during installation
+__version__ = "0.0.3"   # This will be replaced by setuptools_scm during installation
 
 #__all__ = ["Lumos"]
